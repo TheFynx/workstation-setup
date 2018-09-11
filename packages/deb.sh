8a4122673860a9652eb7e6de60d0caf1f40509b6
Levi Smith <levi@fynx.me>
2018-09-10 22:08:44 -0500

    Flushing out deb packages

--- packages/deb.sh
+++ packages/deb.sh
@@ -1,7 +1,62 @@
 info ">>> Removing unwanted packages"
+# apt remove -y
 
 info ">>> Packages succesfully removed"
 
 info ">>> Installing Packages"
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+sudo apt-get install -y \
+    apt-transport-https \
+    curl \
+    wget \
+    unzip \
+    git \
+    ruby \
+    python3 \
+    python3-pip \
+    golang \
+    xclip \
+    pandoc \
+    lynx \
+    neovim \
+    terminator \
+    docker \
+    docker-compose \
+    variety \
+    rsync \
+    libu2f-host \
+    zsh \
+    moka-icon-theme \
+    materia-gtk-theme \
+    diffutils \
+    chromium-browser \
+    slack \
+    vlc \
+    bzip2
+
+info ">>> Installing VS Code"
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt-get update -y
+sudo apt-get install -y code
+
+info ">>> Installing Google Talk Plugin"
+echo "deb http://dl.google.com/linux/talkplugin/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-talkplugin.list
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install google-talkplugin
+
+info ">>> Installing Spotify"
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update -y
+sudo apt-get install spotify-client -y
+
+info ">>> Installing Insomnia"
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee /etc/apt/sources.list.d/insomnia.list
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install insomnia -y
 
 info ">>> Packages succesfully installed"
--- packages/snap.sh
+++ packages/snap.sh
@@ -1,22 +1,29 @@
 if [ -z "$(command -v snap)" ]; then
     if [ "${PKG}" == "apt" ]; then
-        sudo apt -y install snapd > /dev/null 2>&1 || warning ">>> Failed to install Snapcraft using ${PKG}"
+      sudo apt -y install snapd > /dev/null 2>&1 || warning ">>> Failed to install Snapcraft using ${PKG}"
+      sudo systemctl enable snapd
+      sudo systemctl start snapd
     elif [ "${OS}" == "fedora" ]; then
-        sudo dnf install -y snapd > /dev/null 2>&1  || warning ">>> Failed to install Snapcraft using ${PKG}"
-        sudo ln -s /var/lib/snapd/snap /snap || warning ">>> Failed to create snap bin link"
+      sudo dnf install -y snapd > /dev/null 2>&1  || warning ">>> Failed to install Snapcraft using ${PKG}"
+      sudo ln -s /var/lib/snapd/snap /snap || warning ">>> Failed to create snap bin link"
     elif [ "${OS}" == "arch" ]; then
-        yaourt -Sy snapd > /dev/null 2>&1  || warning ">>> Failed to install Snapcraft using ${PKG}"
-        sudo systemctl enable --now snapd.socket > /dev/null 2>&1 || warning ">>> Failed to enable Snapcraft"
+      yaourt -Sy snapd > /dev/null 2>&1  || warning ">>> Failed to install Snapcraft using ${PKG}"
+      sudo systemctl enable --now snapd.socket > /dev/null 2>&1 || warning ">>> Failed to enable Snapcraft"
     else
-        warning ">>> Cannot install snapcraft"
+      warning ">>> Cannot install snapcraft"
     fi
 fi
 
 if [ -n "$(command -v snap)" ]; then
-    sudo snap install postman \
-        insomnia \
-        vlc \
+    if [ "${PKG}" == "apt" ]; then
+      sudo snap install postman \
         simplenote > /dev/null 2>&1
+    else
+      sudo snap install postman \
+          insomnia \
+          vlc \
+          simplenote > /dev/null 2>&1
+    fi
     info ">>> Snaps installed succesfully"
 else
     warning ">>> Snapcraft not installed, cannot install snaps"
